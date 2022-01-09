ff6d772f87cf61830a7257e26344aceec30542ff
GarwelGarwel <okozlovsky@gmail.com>
2022-01-09 16:45:46 +0300

    Added ThermalModExtension for stuff thermal info

---
+++ Source/ThermalModExtension.cs
@@ -0,0 +1,20 @@
+﻿using Verse;
+
+namespace TemperaturesPlus
+{
+    public enum ThingCellInteraction
+    {
+        //None = 0,
+        Separate,
+        Integrated
+    }
+
+    public class ThermalModExtension : DefModExtension
+    {
+        public ThingCellInteraction cellInteraction = ThingCellInteraction.Separate;
+        public bool replacesAirProperties;
+        public float specificHeatCapacity;
+        public float heatCapacity;
+        public float conductivity = 1;
+    }
+}
