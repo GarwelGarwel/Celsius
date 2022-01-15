74248d9b169f98d47493588e3d53be72e8ab0947
GarwelGarwel <okozlovsky@gmail.com>
2022-01-15 20:28:51 +0300

    Added HeatConductivity stat

---
+++ Source/StatWorker_HeatConductivity.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace TemperaturesPlus
+{
+    public class StatWorker_HeatConductivity : StatWorker
+    {
+        public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
+        {
+            CompThermal compThermal = req.Thing?.TryGetComp<CompThermal>();
+            return compThermal != null ? compThermal.ThermalProperties.conductivity : 0;
+        }
+    }
+}
