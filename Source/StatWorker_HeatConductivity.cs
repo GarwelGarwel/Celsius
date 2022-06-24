28dd68331e01be2c302d9eef20b8b1be19f9733c
Garwel <garwel@protonmail.com>
2022-06-24 21:05:15 +0600

    Show effective conductivity with air (instead of building's own conductivity); display heat capacity in kJ/C

--- Source/StatWorker_HeatConductivity.cs
+++ Source/StatWorker_HeatConductivity.cs
@@ -1,4 +1,5 @@
 ﻿using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace Celsius
@@ -8,7 +9,25 @@ namespace Celsius
         public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
         {
             CellThermalProps thermalProps = req.Thing?.TryGetComp<CompThermal>()?.ThermalProperties;
-            return thermalProps != null ? thermalProps.conductivity : 0;
+            if (thermalProps == null)
+                return 0;
+            float conductivity = thermalProps.conductivity * CellThermalProps.Air.conductivity;
+            if (thermalProps.airflow > 0)
+                conductivity *= Mathf.Pow(Settings.ConvectionConductivityEffect, thermalProps.airflow);
+            return conductivity;
+        }
+
+        public override string GetExplanationUnfinalized(StatRequest req, ToStringNumberSense numberSense)
+        {
+            string str = base.GetExplanationUnfinalized(req, numberSense);
+            CellThermalProps thermalProps = req.Thing?.TryGetComp<CompThermal>()?.ThermalProperties;
+            if (thermalProps == null)
+                return str;
+            str += $"\n{req.Thing.LabelCapNoCount} own conductivity: {thermalProps.conductivity.ToStringDecimalIfSmall()}";
+            str += $"\nAir conductivity: x{CellThermalProps.Air.conductivity.ToStringDecimalIfSmall()}";
+            if (thermalProps.airflow > 0)
+                str += $"\nConvection (air flow {thermalProps.airflow.ToStringPercent()}): x{Mathf.Pow(Settings.ConvectionConductivityEffect, thermalProps.airflow).ToStringDecimalIfSmall()}";
+            return str;
         }
     }
 }
