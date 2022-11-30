8ecacfa75611b1597de9fcff6b17179991bd677e
Garwel <garwel@protonmail.com>
2022-11-30 08:30:51 +0100

    Correct, detailed explanation of heat conductivity stat

--- Source/StatWorker_HeatConductivity.cs
+++ Source/StatWorker_HeatConductivity.cs
@@ -9,25 +9,31 @@ namespace Celsius
         public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
         {
             ThermalProps thermalProps = req.Thing?.TryGetComp<CompThermal>()?.ThermalProperties;
-            if (thermalProps == null)
-                return 0;
-            float conductivity = thermalProps.Conductivity * ThermalProps.Air.Conductivity;
-            //if (thermalProps.airflow > 0)
-            //    conductivity *= Mathf.Pow(Settings.ConvectionConductivityEffect, thermalProps.airflow);
-            return conductivity;
+            return thermalProps == null ? 0 : thermalProps.Conductivity;
         }
 
         public override string GetExplanationUnfinalized(StatRequest req, ToStringNumberSense numberSense)
         {
             string str = base.GetExplanationUnfinalized(req, numberSense);
-            ThermalProps thermalProps = req.Thing?.TryGetComp<CompThermal>()?.ThermalProperties;
-            if (thermalProps == null)
+            CompThermal compThermal = req.Thing?.TryGetComp<CompThermal>();
+            if (compThermal == null)
                 return str;
-            str += $"\n{req.Thing.LabelCapNoCount} isolation: {thermalProps.isolation.ToStringPercent()}";
-            str += $"\n{req.Thing.LabelCapNoCount} own conductivity: {thermalProps.Conductivity.ToStringDecimalIfSmall()}";
-            //str += $"\nAir conductivity: x{ThermalProps.Air.Conductivity.ToStringDecimalIfSmall()}";
-            //if (thermalProps.airflow > 0)
-            //    str += $"\nConvection (air flow {thermalProps.airflow.ToStringPercent()}): x{Mathf.Pow(Settings.ConvectionConductivityEffect, thermalProps.airflow).ToStringDecimalIfSmall()}";
+            ThingThermalProperties thingThermalProperties = compThermal.ThingThermalProperties;
+            if (thingThermalProperties == null)
+                return str;
+            str += $"\n{req.Thing.def.LabelCap} isolation: {thingThermalProperties.isolation}";
+            StuffThermalProperties stuffThermalProperties = compThermal.StuffThermalProperties;
+            if (stuffThermalProperties != null)
+                str += $"\nStuff isolation: x{stuffThermalProperties.isolation.ToStringPercent()}";
+            if (thingThermalProperties.airflow > 0)
+                str += $"\nAirflow: {thingThermalProperties.airflow.ToStringPercent()}";
+            if (thingThermalProperties.airflowWhenOpen > 0)
+                str += $"\nAirflow when open: {thingThermalProperties.airflowWhenOpen.ToStringPercent()}";
+            if (compThermal.IsOpen)
+                str += $"\nThe {req.Thing.LabelNoCount} is open".Colorize(Color.yellow);
+            ThermalProps thermalProps = compThermal.ThermalProperties;
+            if (thermalProps != null)
+                str += $"\nConductivity: 2 ^ {-thermalProps.isolation} = {thermalProps.Conductivity.ToStringPercent()}";
             return str;
         }
     }
