ac6ae1629c56698e70929c9017b6f568a51d4d3f
Garwel <garwel@protonmail.com>
2023-05-27 10:36:30 +0200

    Added direct sunlight effect (-3 to comfortable temperature), see issue # 33

---
+++ Source/StatPart_DirectSunlight.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Celsius
+{
+    public class StatPart_DirectSunlight : StatPart
+    {
+        public float minGlow = 1;
+        public float offset;
+
+        public bool AtDirectSunlight(Thing thing) =>
+            thing != null
+            && thing.Spawned
+            && thing.Map != null
+            && thing.Map.skyManager.CurSkyGlow >= minGlow
+            && thing.Map.weatherManager.curWeather == WeatherDefOf.Clear
+            && !thing.Position.Roofed(thing.Map);
+
+        bool AppliesTo(StatRequest req) => Settings.PawnWeatherEffects && offset != 0 && AtDirectSunlight(req.Thing);
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (AppliesTo(req))
+                val += offset;
+        }
+
+        public override string ExplanationPart(StatRequest req) =>
+            AppliesTo(req)
+                ? "Celsius_StatPart_DirectSunlight_Explanation".Translate(offset.ToStringTemperatureOffset())
+                : null;
+    }
+}
