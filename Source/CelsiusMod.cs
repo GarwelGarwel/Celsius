4498ce1f6316bf5bf6abd60ad1d0e0fe35785308
Garwel <garwel@protonmail.com>
2023-05-28 12:44:04 +0200

    Updated Insulation stat description localization keys, removed references to Si units

--- Source/CelsiusMod.cs
+++ Source/CelsiusMod.cs
@@ -72,11 +72,6 @@ namespace Celsius
             content.Label("Celsius_Settings_UpdateInterval".Translate(TicksPerUpdate), tooltip: "Celsius_Settings_UpdateInterval_tooltip".Translate(TicksPerUpdate_Default));
             TicksPerUpdate = (int)content.Slider(TicksPerUpdate, 60, 300) / 20 * 20;
 
-            // Localization key: Celsius_Settings_ConductivitySpeed - Conductivity speed: {(1 / Mathf.Log(ConductivityPowerBase, ConductivityPowerBase_Default)).ToStringPercent()}
-            //                   Celsius_Settings_ConductivitySpeed_tooltip - How quickly temperature changes.
-            //content.Label($"Celsius_Settings_ConductivitySpeed".Translate((1 / Mathf.Log(ConductivityPowerBase, ConductivityPowerBase_Default)).ToStringPercent()), tooltip: "Celsius_Settings_ConductivitySpeed_tooltip".Translate());
-            //ConductivityPowerBase = (float)Math.Round(content.Slider(ConductivityPowerBase, 0.1f, 0.9f), 2);
-
             // Localization key: Celsius_Settings_ConvectionConductivityEffect - Convection conductivity effect: x{ConvectionConductivityEffect}
             //                   Celsius_Settings_ConvectionConductivityEffect_tooltip - How much air convection increases air conductivity. Recommended value: {ConvectionConductivityEffect_Default}.
             content.Label($"{"Celsius_Settings_ConvectionConductivityEffect".Translate()} x{ConvectionConductivityEffect}", tooltip: "Celsius_Settings_ConvectionConductivityEffect_tooltip".Translate(ConvectionConductivityEffect_Default));
--- Source/StatWorker_Insulation.cs
+++ Source/StatWorker_Insulation.cs
@@ -44,18 +44,18 @@ namespace Celsius
             if (thermalProps == null)
                 return base.GetExplanationUnfinalized(req, numberSense);
 
-            StringBuilder explanation = new StringBuilder("Celsius_Stat_HeatConductivity_BaseInsulation"
+            StringBuilder explanation = new StringBuilder("Celsius_Stat_Insulation_BaseInsulation"
                 .Translate(req.Def.label, thingThermalProperties.insulation.ToString("F1"))
                 .CapitalizeFirst());
             if (stuffThermalProperties != null)
             {
-                explanation.AppendInNewLine("Celsius_Stat_HeatConductivity_StuffInsulation".Translate(req.StuffDef.label).CapitalizeFirst());
+                explanation.AppendInNewLine("Celsius_Stat_Insulation_StuffInsulation".Translate(req.StuffDef.label).CapitalizeFirst());
                 explanation.Append($"x{stuffThermalProperties.insulationFactor.ToStringPercent()}");
             }
             if (isOpen && thingThermalProperties.airflow != thingThermalProperties.airflowWhenOpen)
-                explanation.AppendInNewLine("Celsius_Stat_HeatConductivity_AirflowOpen".Translate(req.Def.label).Colorize(Color.yellow).CapitalizeFirst());
+                explanation.AppendInNewLine("Celsius_Stat_Insulation_AirflowOpen".Translate(req.Def.label).Colorize(Color.yellow).CapitalizeFirst());
             if (thermalProps.airflow != 0)
-                explanation.AppendInNewLine("Celsius_Stat_HeatConductivity_Airflow".Translate(thermalProps.airflow.ToStringPercent()));
+                explanation.AppendInNewLine("Celsius_Stat_Insulation_Airflow".Translate(thermalProps.airflow.ToStringPercent()));
 
             return explanation.ToString();
         }
