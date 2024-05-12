ccb352615f8b18a2bcf261b53019f1c8c883296b
GarwelGarwel <garwel@protonmail.com>
2024-05-12 12:06:50 +0200

    Fixed NRE in explanations for heat capacity and insulation stats; improved stuff detection

--- Source/TemperatureUtility.cs
+++ Source/TemperatureUtility.cs
@@ -151,33 +151,6 @@ namespace Celsius
             return true;
         }
 
-        public static float GetAverageSnowDepth(this Map map)
-        {
-            float snowDepth = map.snowGrid.TotalDepth;
-            if (snowDepth <= 0)
-                return 0;
-            int possibleSnowCells = 0;
-            for (int i = 0; i < map.Size.x * map.Size.z; i++)
-            {
-                TerrainDef terrain = map.terrainGrid.TerrainAt(i);
-                if (terrain != null && !terrain.holdSnow)
-                    continue;
-                Building building = map.edificeGrid[i];
-                if (building != null && !SnowGrid.CanCoexistWithSnow(building.def))
-                    continue;
-                if (map.roofGrid.Roofed(i))
-                    continue;
-                possibleSnowCells++;
-            }
-            if (possibleSnowCells > 0)
-            {
-                snowDepth /= possibleSnowCells;
-                LogUtility.Log($"Covering frozen terrain with average snow level of {snowDepth:F4}.");
-                return snowDepth;
-            }
-            return 0;
-        }
-
         #endregion MISC UTILITIES
     }
 }
