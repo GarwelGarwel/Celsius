87352dd91b4f18cf6cbdaf65be0b84bc17d4f339
Benjamin Prottengeier <benni007.pro@gmail.com>
2024-07-01 16:17:56 +0200

    More fast = More good
    -Applied the neighbor optimizations from the multithreaded function to the single threaded one. Big speedups on my pc, it's at least 30% faster
    -Applied some minor "optimization" to the thermal property getter. I found that when you're accessing an array twice, caching the result is faster. In this case, it didn't do much, but that could be because of my CPU cache fetching it anyway
    -Brought back the logging checks for the multithreaded implementation. I personally couldn't care less, but the performance impact seems to be negligible and it *is* a feature of the regular implementation
    -Made the Unity job threading checkbox not display when threading is disabled

--- Source/Workers.cs
+++ Source/Workers.cs
@@ -7,6 +7,7 @@ using JetBrains.Annotations;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
+using Verse;
 
 namespace Celsius
 {
@@ -14,17 +15,19 @@ namespace Celsius
     internal struct RegularWorkerReference : IJobParallelFor
     {
         public readonly Tuple<int, int>[] arr;
-        public TemperatureInfo info;
+        public readonly TemperatureInfo info;
+        public readonly int cell;
 
-        public RegularWorkerReference(Tuple<int, int>[] arr, TemperatureInfo info)
+        public RegularWorkerReference(Tuple<int, int>[] arr, TemperatureInfo info, int cell)
         {
             this.arr = arr;
             this.info = info;
+            this.cell = cell;
         }
         public void Execute(int index)
         {
             var tuple = arr[index];
-            info.ProcessColumns(tuple.Item1, tuple.Item2);
+            info.ProcessColumns(tuple.Item1, tuple.Item2, cell);
         }
     }
 }
